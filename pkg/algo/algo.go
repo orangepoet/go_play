f412d10d5747e3351d4719a8cfdf637acf03039e
orangepoet <orangepoet@qq.com>
2024-02-04 00:08:49 +0800

    algo

--- pkg/algo/algo.go
+++ pkg/algo/algo.go
@@ -28,12 +28,11 @@ func deleteDuplicates(head *ListNode) *ListNode {
 }
 
 func makeList(arr []int) *ListNode {
-	var next ListNode
+	var next *ListNode
 	for i := len(arr) - 1; i >= 0; i-- {
-		cur := ListNode{arr[i], &next}
-		next = cur
+		next = &ListNode{arr[i], next}
 	}
-	return &next
+	return next
 }
 
 func (head *ListNode) toArr() []int {
@@ -65,4 +64,81 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func resolvePrime(n int) []int {
+	ans := make([]int, 0)
+
+	for left, f := n, 2; left > 2 && f <= left; {
+		if left%f == 0 {
+			ans = append(ans, f)
+			left = left / f
+		} else {
+			f++
+		}
+	}
+	return ans
+}
+
+func comb(n, m, k int) [][]int {
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = i + 1
+	}
+	ans := make([][]int, 0)
+	dfs(arr, m, k, 0, make([]int, 0), 0, &ans)
+	return ans
+}
+
+func dfs(arr []int, left int, k int, sum int, c []int, next int, ans *[][]int) {
+	if left == 0 && k == sum {
+		*ans = append(*ans, c)
+		return
+	}
+	for i := next; i < len(arr); i++ {
+		dfs(arr, left-1, k, sum+arr[i], append(c, arr[i]), i+1, ans)
+	}
+}
+
+func isValid(s string) bool {
+	left, right := '{', '}'
+	stack := make([]rune, 0)
+	for _, c := range s {
+		if c == left {
+			stack = append(stack, c)
+		} else if c == right {
+			top := len(stack) - 1
+			if top < 0 {
+				return false
+			}
+			if stack[top] == left {
+				stack = stack[:top]
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func mergeListNode(list []*ListNode) *ListNode {
+	dummy := &ListNode{}
+	prev := dummy
+
+	for {
+		selected := 0
+		for i := range list {
+			if list[i] == nil {
+				continue
+			}
+			if list[selected] == nil || list[i].Val < list[selected].Val {
+				selected = i
+			}
+		}
+		if list[selected] == nil {
+			break
+		}
+		prev.Next = list[selected]
+		prev = list[selected]
+		list[selected] = list[selected].Next
+	}
+	return dummy.Next
 }
--- pkg/algo/algo_test.go
+++ pkg/algo/algo_test.go
@@ -2,6 +2,7 @@
 package algo
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -26,5 +27,69 @@ func TestDeleteDuplicates(t *testing.T) {
 	r := deleteDuplicates(makeList([]int{1, 2, 3, 3, 4, 4, 5}))
 	if !equals(r.toArr(), []int{1, 2, 5}) {
 		t.Errorf("expected {1,2,5} but %d", r.toArr())
+	}
+}
+
+func TestResolvePrime(t *testing.T) {
+	testCase := map[int][]int{
+		10: {2, 5}, 20: {2, 2, 5}, 30: {2, 3, 5}, 35: {5, 7},
+	}
+	for k, v := range testCase {
+		r := resolvePrime(k)
+		if !reflect.DeepEqual(r, v) {
+			t.Errorf("expected %v but %v", v, r)
+		}
+	}
+}
+
+func TestComb(t *testing.T) {
+	ret := comb(9, 2, 12)
+	expected := [][]int{{3, 9}, {4, 8}, {5, 7}}
+	if !reflect.DeepEqual(ret, expected) {
+		t.Errorf("expected %v but %v", expected, ret)
+	}
+}
+
+func Test_isValid(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test1", args: args{s: "{{}}"}, want: true},
+		{name: "test2", args: args{s: "{{}"}, want: false},
+		{name: "test3", args: args{s: "}{"}, want: false},
+		{name: "test3", args: args{s: "{}{"}, want: false},
+		{name: "test3", args: args{s: "{}{}"}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValid(tt.args.s); got != tt.want {
+				t.Errorf("isValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_mergeListNode(t *testing.T) {
+	type args struct {
+		list []*ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{name: "test1", args: args{list: []*ListNode{makeList([]int{1, 2, 3}), makeList([]int{2, 3, 4})}}, want: makeList([]int{1, 2, 2, 3, 3, 4})},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeListNode(tt.args.list); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeListNode() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
