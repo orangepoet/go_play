f03fb9cac560ec4c3bc71f289ac22fdbd1146e42
orangepoet <orangepoet@qq.com>
2025-03-05 09:55:18 +0800

    tmp save

---
+++ cmd/go_play/blevel_test.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/analysis"
+	"github.com/blevesearch/bleve/v2/analysis/analyzer/custom"
+	"github.com/blevesearch/bleve/v2/analysis/tokenizer/unicode"
+	"github.com/blevesearch/bleve/v2/registry"
+	"github.com/go-ego/gse"
+)
+
+func TestBleveCase1(t *testing.T) {
+	// 创建一个内存索引
+	mapping := bleve.NewIndexMapping()
+
+	// 添加自定义分词器
+	err := mapping.AddCustomTokenizer("unicode", map[string]interface{}{
+		"type": unicode.Name,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = mapping.AddCustomAnalyzer("chinese", map[string]interface{}{
+		"type":      custom.Name,
+		"tokenizer": "unicode", // 使用 segment 分词器
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mapping.DefaultAnalyzer = "chinese"
+
+	index, err := bleve.NewMemOnly(mapping)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 索引文档
+	doc1 := map[string]interface{}{
+		"content": "我是一个好学生",
+	}
+	err = index.Index("1", doc1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc2 := map[string]interface{}{
+		"content": "我才是一个坏学生",
+	}
+	err = index.Index("2", doc2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询 "好学生"
+	query := bleve.NewMatchQuery("好学生")
+	searchRequest := bleve.NewSearchRequest(query)
+	searchResult, err := index.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 输出查询结果
+	fmt.Printf("查询 '好学生' 的结果: %v\n", searchResult)
+}
+
+// 注册自定义分词器
+func init() {
+	registry.RegisterTokenizer("gse", func(config map[string]interface{}, cache *registry.Cache) (analysis.Tokenizer, error) {
+		return NewGseTokenizer(), nil
+	})
+}
+
+// GseTokenizer 是一个自定义的分词器，基于 gse
+type GseTokenizer struct {
+	segmenter *gse.Segmenter
+}
+
+func NewGseTokenizer() *GseTokenizer {
+	segmenter := &gse.Segmenter{}
+	segmenter.LoadDict() // 加载默认字典
+	return &GseTokenizer{
+		segmenter: segmenter,
+	}
+}
+
+func (t *GseTokenizer) Tokenize(input []byte) analysis.TokenStream {
+	// 使用 gse 分词
+	segments := t.segmenter.Cut(string(input), true)
+
+	// 将分词结果转换为 analysis.Token
+	tokens := make(analysis.TokenStream, 0, len(segments))
+	for _, seg := range segments {
+		tokens = append(tokens, &analysis.Token{
+			Term: []byte(seg),
+		})
+	}
+	return tokens
+}
+
+func TestBleveCase2(t *testing.T) {
+	// 创建自定义分词器
+	mapping := bleve.NewIndexMapping()
+
+	// // 注册自定义分词器
+	// err := mapping.AddCustomTokenizer("gse", map[string]interface{}{
+	// 	"type": "gse",
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // 注册自定义分析器
+	// err = mapping.AddCustomAnalyzer("chinese", map[string]interface{}{
+	// 	"type":      custom.Name,
+	// 	"tokenizer": "gse", // 使用自定义的 gse 分词器
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// mapping.DefaultAnalyzer = "chinese"
+
+	// 创建索引
+	index, err := bleve.NewMemOnly(mapping)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 索引文档
+	doc1 := map[string]interface{}{
+		"content": "我是一个好学生",
+	}
+	err = index.Index("1", doc1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc2 := map[string]interface{}{
+		"content": "我是一个坏学生",
+	}
+	err = index.Index("2", doc2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查询 "好学生"
+	query := bleve.NewMatchPhraseQuery("好学生")
+	searchRequest := bleve.NewSearchRequest(query)
+	searchResult, err := index.Search(searchRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 输出查询结果
+	fmt.Printf("查询 '好学生' 的结果: %v\n", searchResult)
+}
--- internal/algo.go
+++ internal/algo.go
@@ -5,33 +5,36 @@ import (
 	"strings"
 )
 
-// ListNode 链表节点
+// ListNode 定义链表节点结构
 type ListNode struct {
-	Val  int
-	Next *ListNode
+	Val  int       // 节点值
+	Next *ListNode // 指向下一个节点的指针
 }
 
-// canJump: jump game
+// canJump 判断是否能跳到最后一个位置
+// nums: 每个位置可以跳跃的最大长度
 func canJump(nums []int) bool {
-	m := 0
+	m := 0 // 当前能到达的最远距离
 	last := len(nums)
 	for idx, e := range nums {
-		if idx > m {
+		if idx > m { // 如果当前位置超过了能到达的最远距离，返回false
 			return false
 		}
 		next := idx + e
-		if next >= last {
+		if next >= last { // 如果能跳到或超过终点，返回true
 			return true
 		}
-		m = max(m, next)
+		m = max(m, next) // 更新最远可达距离
 	}
 	return m > len(nums)
 }
 
+// deleteDuplicates 删除链表中的重复元素
 func deleteDuplicates(head *ListNode) *ListNode {
 	return head
 }
 
+// makeList 通过数组创建链表
 func makeList(arr []int) *ListNode {
 	var next *ListNode
 	for i := len(arr) - 1; i >= 0; i-- {
@@ -40,6 +43,7 @@ func makeList(arr []int) *ListNode {
 	return next
 }
 
+// toArr 将链表转换为数组
 func (head *ListNode) toArr() []int {
 	r := []int{}
 	for head != nil {
@@ -49,6 +53,7 @@ func (head *ListNode) toArr() []int {
 	return r
 }
 
+// equals 判断两个整数数组是否相等
 func equals(a, b []int) bool {
 	if a == nil && b == nil {
 		return true
@@ -64,6 +69,7 @@ func equals(a, b []int) bool {
 	return true
 }
 
+// max 返回两个整数中的较大值
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -71,6 +77,7 @@ func max(a, b int) int {
 	return b
 }
 
+// min 返回两个整数中的较小值
 func min(a, b int) int {
 	if a < b {
 		return a
@@ -78,6 +85,7 @@ func min(a, b int) int {
 	return b
 }
 
+// resolvePrime 分解质因数
 func resolvePrime(n int) []int {
 	ans := make([]int, 0)
 
@@ -92,6 +100,8 @@ func resolvePrime(n int) []int {
 	return ans
 }
 
+// comb 计算组合数
+// n: 总数, m: 选择数量, k: 目标和
 func comb(n, m, k int) [][]int {
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
@@ -102,6 +112,7 @@ func comb(n, m, k int) [][]int {
 	return ans
 }
 
+// dfs 深度优先搜索辅助函数
 func dfs(arr []int, left int, k int, sum int, c []int, next int, ans *[][]int) {
 	if left == 0 && k == sum {
 		*ans = append(*ans, c)
@@ -112,6 +123,7 @@ func dfs(arr []int, left int, k int, sum int, c []int, next int, ans *[][]int) {
 	}
 }
 
+// isValid 判断括号是否有效
 func isValid(s string) bool {
 	left, right := '{', '}'
 	stack := make([]rune, 0)
@@ -131,6 +143,7 @@ func isValid(s string) bool {
 	return len(stack) == 0
 }
 
+// mergeListNode 合并多个有序链表
 func mergeListNode(list []*ListNode) *ListNode {
 	dummy := &ListNode{}
 	prev := dummy
@@ -155,12 +168,14 @@ func mergeListNode(list []*ListNode) *ListNode {
 	return dummy.Next
 }
 
+// TreeNode 定义二叉树节点结构
 type TreeNode struct {
 	val   int
 	left  *TreeNode
 	right *TreeNode
 }
 
+// preOrder 前序遍历二叉树
 func preOrder(tn *TreeNode) []int {
 	if tn == nil {
 		return nil
@@ -168,15 +183,15 @@ func preOrder(tn *TreeNode) []int {
 	ans := []int{tn.val}
 	ans = append(ans, preOrder(tn.left)...)
 	ans = append(ans, preOrder(tn.right)...)
-
 	return ans
 }
 
+// addList 两个链表代表的数相加
 func addList(left, right *ListNode) *ListNode {
 	dummp := &ListNode{}
 	p := dummp
 
-	proceed := false
+	proceed := false // 进位标志
 	for left != nil || right != nil {
 		sum := 0
 		if left != nil {
@@ -205,12 +220,14 @@ func addList(left, right *ListNode) *ListNode {
 	return dummp.Next
 }
 
+// generateParenthesis 生成有效的括号组合
 func generateParenthesis(n int) []string {
 	ans := make([]string, 0)
 	generateParenthesis0(&ans, n, n, "")
 	return ans
 }
 
+// generateParenthesis0 生成括号的递归辅助函数
 func generateParenthesis0(ans *[]string, l, r int, s string) {
 	if l == 0 && r == 0 {
 		*ans = append(*ans, s)
@@ -224,6 +241,7 @@ func generateParenthesis0(ans *[]string, l, r int, s string) {
 	}
 }
 
+// removeNthFromEnd 删除链表倒数第n个节点
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	dummy := &ListNode{Next: head}
 	cur, next := dummy, head
@@ -239,6 +257,7 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	return dummy.Next
 }
 
+// makeChange 计算凑出目标金额所需的最少硬币数
 func makeChange(n int, coins []int) int {
 	dp := make([]int, n+1)
 	dp[0] = 0
@@ -258,8 +277,10 @@ func makeChange(n int, coins []int) int {
 	return dp[n]
 }
 
+// patternMatch 模式匹配
+// pattern: 模式串
+// words: 待匹配的字符串
 func patternMatch(pattern, words string) bool {
-	// "abba"  | "dog cat cat dog"
 	mapping := make(map[string]uint8)
 	stored := make(map[uint8]bool)
 	wordSlice := strings.Split(words, " ")
@@ -279,6 +300,7 @@ func patternMatch(pattern, words string) bool {
 	return true
 }
 
+// maxDepth 计算二叉树的最大深度
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
@@ -286,10 +308,12 @@ func maxDepth(root *TreeNode) int {
 	return 1 + max(maxDepth(root.left), maxDepth(root.right))
 }
 
+// isMirror 判断二叉树是否对称
 func isMirror(node *TreeNode) bool {
 	return isMirror0(node, node)
 }
 
+// isMirror0 判断对称性的递归辅助函数
 func isMirror0(l, r *TreeNode) bool {
 	if l == nil && r == nil {
 		return true
@@ -300,6 +324,7 @@ func isMirror0(l, r *TreeNode) bool {
 	return l.val == r.val && isMirror0(l.left, r.right) && isMirror0(l.right, r.left)
 }
 
+// minPathSum 计算网格的最小路径和
 func minPathSum(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
 	dp := make([][]int, m)
@@ -307,13 +332,16 @@ func minPathSum(grid [][]int) int {
 		dp[i] = make([]int, n)
 	}
 	dp[0][0] = grid[0][0]
+	// 初始化第一列
 	for i := 1; i < m; i++ {
 		dp[i][0] = dp[i-1][0] + grid[i][0]
 	}
+	// 初始化第一行
 	for j := 1; j < n; j++ {
 		dp[0][j] = dp[0][j-1] + grid[0][j]
 	}
 
+	// 动态规划填充其余位置
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
 			dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
@@ -322,6 +350,7 @@ func minPathSum(grid [][]int) int {
 	return dp[m-1][n-1]
 }
 
+// searchRange 在排序数组中查找元素的起始和结束位置
 func searchRange(nums []int, target int) []int {
 	low, high := 0, len(nums)-1
 	for low <= high {
@@ -332,9 +361,11 @@ func searchRange(nums []int, target int) []int {
 			high = mid - 1
 		} else {
 			start, end := mid, mid
+			// 向左扩展
 			for start-1 >= 0 && nums[start-1] == target {
 				start--
 			}
+			// 向右扩展
 			for end+1 <= len(nums)-1 && nums[end+1] == target {
 				end++
 			}
@@ -344,7 +375,7 @@ func searchRange(nums []int, target int) []int {
 	return []int{-1, -1}
 }
 
-// public List<Integer> rightSideView(TreeNode root) {
+// rightSideView 获取二叉树的右视图
 func rightSideView(root *TreeNode) []int {
 	ret := make([]int, 0)
 
@@ -372,6 +403,7 @@ func rightSideView(root *TreeNode) []int {
 	return ret
 }
 
+// lengthOfLongestSubstring 计算最长不含重复字符的子串长度
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
@@ -390,10 +422,10 @@ func lengthOfLongestSubstring(s string) int {
 		}
 		m[x] = end
 	}
-
 	return max
 }
 
+// numIslands 计算岛屿数量
 func numIslands(grid [][]int) int {
 	if len(grid) == 0 || len(grid[0]) == 0 {
 		return 0
@@ -411,6 +443,7 @@ func numIslands(grid [][]int) int {
 	return count
 }
 
+// numIslands0 深度优先搜索标记连通的岛屿
 func numIslands0(grid [][]int, x, y, xl, yl int) {
 	if x >= xl || y >= yl {
 		return
@@ -423,7 +456,8 @@ func numIslands0(grid [][]int, x, y, xl, yl int) {
 	numIslands0(grid, x, y+1, xl, yl)
 }
 
-// WeightSelect 权重选取
+// WeightSelect 根据权重随机选择
+// vw: 值-权重映射表
 func weightSelect(vw map[int]int) (int, bool) {
 	total := 0
 	for _, w := range vw {
